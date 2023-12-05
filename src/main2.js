0dbb96a0d5acdf4c294435fb216652dfe8f9a222
ArydaLohany <arydalohanytp@gmail.com>
2023-12-05 16:52:08 -0300

    fix: FIXED

--- src/main2.js
+++ src/main2.js
@@ -36,29 +36,130 @@ formCloseBtn.addEventListener("click", () => {
 });
 });*/
 
-let btn = document.querySelector('#verSenha')
+let nome = document.querySelector('#nome');
+let labelNome = document.querySelector('#labelNome');
+
+let usuario = document.querySelector('#usuarioCad');
+let labelUsuario = document.querySelector('#labelUsuario');
+let validUsuario = false;
+
+let validNome = false;
+
+let btn = document.querySelector('#verSenha');
+let btnConfirm = document.querySelector('#verConfirmSenha');
+
+let senha = document.querySelector('#senha');
+let labelSenha = document.querySelector('#labelSenha');
+let validSenha = false;
+
+let confirmSenha = document.querySelector('#confirmSenha');
+let labelConfirmSenha = document.querySelector('#labelConfirmSenha');
+let validConfirmSenha = false;
+
+let msgError = document.querySelector('#msgError');
+let msgSuccess = document.querySelector('#msgSuccess');
+
+nome.addEventListener('keyup', () => {
+  if (nome.value.length <= 2) {
+    labelNome.style.color = 'red';
+    labelNome.textContent = 'Nome *Insira no mínimo 3 caracteres';
+    nome.style.borderColor = 'red';
+    validNome = false;
+  } else {
+    labelNome.style.color = 'green';
+    labelNome.textContent = 'Nome';
+    nome.style.borderColor = 'green';
+    validNome = true;
+  }
+});
+
+usuario.addEventListener('keyup', () => {
+  if (usuario.value.length <= 4) {
+    labelUsuario.style.color = 'red';
+    labelUsuario.textContent = 'Usuário *Insira no mínimo 5 caracteres';
+    usuario.style.borderColor = 'red';
+    validUsuario = false;
+  } else {
+    labelUsuario.style.color = 'green';
+    labelUsuario.textContent = 'Usuário';
+    usuario.style.borderColor = 'green';
+    validUsuario = true;
+  }
+});
+
+senha.addEventListener('keyup', () => {
+  if (senha.value.length <= 5) {
+    labelSenha.style.color = 'red';
+    labelSenha.textContent = 'Senha *Insira no mínimo 6 caracteres';
+    senha.style.borderColor = 'red';
+    validSenha = false;
+  } else {
+    labelSenha.style.color = 'green';
+    labelSenha.textContent = 'Senha';
+    senha.style.borderColor = 'green';
+    validSenha = true;
+  }
+});
+
+confirmSenha.addEventListener('keyup', () => {
+  if (senha.value !== confirmSenha.value) {
+    labelConfirmSenha.style.color = 'red';
+    labelConfirmSenha.textContent = 'Confirmar Senha *As senhas não conferem';
+    confirmSenha.style.borderColor = 'red';
+    validConfirmSenha = false;
+  } else {
+    labelConfirmSenha.style.color = 'green';
+    labelConfirmSenha.textContent = 'Confirmar senha';
+    confirmSenha.style.borderColor = 'green';
+    validConfirmSenha = true;
+  }
+});
+
+function logar() {
+  if (validNome && validUsuario && validSenha && validConfirmSenha) {
+    let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]');
+
+    listaUser.push({
+      nomeCad: nome.value,
+      userCad: usuario.value,
+      senhaCad: senha.value
+    });
+
+    localStorage.setItem('listaUser', JSON.stringify(listaUser));
+
+    msgSuccess.style.display = 'block';
+    msgSuccess.innerHTML = '<strong>Cadastrando usuário...</strong>';
+    msgError.style.display = 'none';
+    msgError.innerHTML = '';
+
+    setTimeout(() => {
+      window.location.href = 'https://cdpn.io/thicode/debug/ZELzYxV/dXAqBaRyvwJk';
+    }, 3000);
+  } else {
+    msgError.style.display = 'block';
+    msgError.innerHTML = '<strong>Preencha todos os campos corretamente antes de logar</strong>';
+    msgSuccess.innerHTML = '';
+    msgSuccess.style.display = 'none';
+  }
+}
 
 btn.addEventListener('click', () => {
-    let inputSenha = document.querySelector('#senha')
+  let inputSenha = document.querySelector('#senha');
 
-    if(inputSenha.getAttribute('type') == 'password'){
-        inputSenhasetAttribute('type', 'text')
-    } else {
-        inputSenha.setAttribute('type', 'password')
-    }
-})
+  if (inputSenha.getAttribute('type') === 'password') {
+    inputSenha.setAttribute('type', 'text');
+  } else {
+    inputSenha.setAttribute('type', 'password');
+  }
+});
 
-let btnConfirm = document.querySelector('#verConfirmSenha')
-btnConfirm.addEventListener('click', ()=>{
-    let inputConfirmSenha = document.querySelector('#confirmSenha')
+btnConfirm.addEventListener('click', () => {
+  let inputConfirmSenha = document.querySelector('#confirmSenha');
 
-    if(inputConfirmSenha.getAttribute('type') == 'password'){
-        inputConfirmSenha.setAttribute('type', 'text')  
-    } else {
-        inputConfirmSenha.setAttribute('type', 'password')
-    }
-})
+  if (inputConfirmSenha.getAttribute('type') === 'password') {
+    inputConfirmSenha.setAttribute('type', 'text');
+  } else {
+    inputConfirmSenha.setAttribute('type', 'password');
+  }
+});
 
-function cadastrar(){
-    alert('clicado')
-}
