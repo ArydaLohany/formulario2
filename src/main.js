567f6a10f1efe5b9b223101ea6a3a88c4c86d15c
ArydaLohany <arydalohanytp@gmail.com>
2023-12-12 16:52:18 -0300

    little changes on the order

--- src/main.js
+++ src/main.js
@@ -183,6 +183,7 @@ mode_icon .addEventListener('change', () => {
   document.body.classList.toggle('dark')
 })*/
 
+
 function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
